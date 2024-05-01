ed08b83f708157722f728e1a7de7efda2ad97c64
Lavanya Kasturia <KasturiaL@cardiff.ac.uk>
2024-05-01 01:38:27 +0100

    Updated config

--- src/app/shop.py
+++ src/app/shop.py
@@ -1,9 +1,15 @@
 from flask import (Flask, render_template, redirect, url_for,
                    json, request)
 from flask_bootstrap import Bootstrap
+import os
+
+cur_path = os.path.dirname(__file__)
+src = os.path.abspath(os.path.join(cur_path, os.pardir))
+parent = os.path.abspath(os.path.join(src, os.pardir))
+config = os.path.join(parent, 'config', 'config.cfg')
 
 app = Flask(__name__)
-app.config.from_pyfile('.\\config\\config.cfg')
+app.config.from_pyfile(config)
 bootstrap = Bootstrap(app)
 
 
