481189150aed7de8cee9be2e3ac6ec7434c2678b
Lavanya Kasturia <KasturiaL@cardiff.ac.uk>
2024-05-01 22:11:44 +0100

    reworked imports and app creation

--- src/app/shop.py
+++ src/app/shop.py
@@ -1,16 +1,7 @@
-from flask import (Flask, render_template, redirect, url_for,
-                   json, request)
-from flask_bootstrap import Bootstrap
-import os
-
-cur_path = os.path.dirname(__file__)
-src = os.path.abspath(os.path.join(cur_path, os.pardir))
-parent = os.path.abspath(os.path.join(src, os.pardir))
-config = os.path.join(parent, 'config', 'config.cfg')
-
-app = Flask(__name__)
-app.config.from_pyfile(config)
-bootstrap = Bootstrap(app)
+from create_app import app
+from flask import (Flask, render_template, redirect, url_for, request)
+from instance.db.initDB import dbCheck
+dbCheck()
 
 
 @app.route('/faq')
