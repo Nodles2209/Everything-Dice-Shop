72305af7f9171164b1bede7b177ec4c458dd3ed7
Lavanya Kasturia <KasturiaL@cardiff.ac.uk>
2024-05-01 05:55:54 +0100

    Creates and initialises database with basic table data and items

--- instance/db/initDB.py
+++ instance/db/initDB.py
@@ -1,7 +1,8 @@
 import createDB
-from src.app.shop import cur_path, src, parent
+from src.app.shop import parent
 import os
 from flask import json
+import datetime
 
 with createDB.app.app_context():
     createDB.db.create_all()
@@ -13,42 +14,37 @@ item_data = json.load(open(items_file, encoding='utf-8'))
 lookup_data = json.load(open(lookup_file, encoding='utf-8'))
 
 with createDB.app.app_context():
-    for category in lookup_data:
-        for category_name in lookup_data[category]:
-            newCategory = createDB.ItemClassification(classification_id=category_name["classification_id"],
-                                                      id_identifier=category_name["id_identifier"],
-                                                      category_name=category_name["category_name"])
-            createDB.db.session.add(newCategory)
+    for classification in lookup_data["Item classifications"]:
+        newCategory = createDB.ItemClassification(id_identifier=classification["id_identifier"],
+                                                  category_name=classification["category_name"])
+        createDB.db.session.add(newCategory)
 
-        for country in lookup_data["Country list"]:
-            newCountry = createDB.CountryList(country_id=country["country_id"],
-                                              country_name=country["country_name"])
-            createDB.db.session.add(newCountry)
+    for country in lookup_data["Country list"]:
+        newCountry = createDB.CountryList(country_name=country["country_name"])
+        createDB.db.session.add(newCountry)
 
-        for vendor in lookup_data["Card classification"]:
-            newVendor = createDB.CardClassification(classification_id=vendor["classification_id"],
-                                                    id_identifier=vendor["id_identifier"],
-                                                    card_type=vendor["card_type"])
-            createDB.db.session.add(newVendor)
+    for vendor in lookup_data["Card classification"]:
+        newVendor = createDB.CardClassification(card_identifier=vendor["card_identifier"],
+                                                card_type=vendor["card_type"])
+        createDB.db.session.add(newVendor)
 
     for category in item_data:
         for item in item_data[category]:
-            newListing = createDB.ItemListing(listing_id=item["listing_id"],
-                                              id_identifier=item["listing_id"][0],
-                                              listing_name=item["listing_name"],
+            newListing = createDB.ItemListing(id_identifier=str(item["listing_id"])[0],
                                               thumbnail_img=item["thumbnail_img"],
+                                              listing_name=item["listing_name"],
                                               listing_description=item["listing_description"],
-                                              avg_price=item["avg_price"], avg_rating=item["avg_rating"],
+                                              avg_price=item["avg_price"],
+                                              avg_rating=item["avg_rating"],
                                               num_of_reviews=item["num_of_reviews"], footprint=item["footprint"])
 
             creating_table_for = item["options"]
             for index in range(len(creating_table_for["option_id"])):
-                newOption = createDB.ListingOptions(option_id=creating_table_for["option_id"][index],
-                                                    for_listing_id=item["listing_id"],
-                                                    option_name=creating_table_for["option_name"][index],
+                newOption = createDB.ListingOptions(for_listing_id=item["listing_id"],
                                                     option_img=creating_table_for["option_img"][index],
+                                                    option_name=creating_table_for["option_name"][index],
                                                     option_price=creating_table_for["option_price"][index],
-                                                    options_stock=creating_table_for["options_stock"][index],
+                                                    option_stock=creating_table_for["option_stock"][index],
                                                     option_sold=creating_table_for["option_sold"][index],
                                                     option_inStock=creating_table_for["option_inStock"][index])
                 newListing.options.append(newOption)
@@ -66,8 +62,8 @@ with createDB.app.app_context():
             creating_table_for = item["replies"]
             for index in range(len(creating_table_for["reply_id"])):
                 newReply = createDB.ReviewReplies(reply_id=creating_table_for["reply_id"][index],
-                                                  under_review_id=creating_table_for["review_id"][index],
                                                   reply_user_id=creating_table_for["reply_user_id"][index],
+                                                  under_review_id=creating_table_for["review_id"][index],
                                                   reply_text=creating_table_for["reply_text"][index])
                 newReview.replies.append(newReply)
                 newListing.reviews.append(newReview)
@@ -76,9 +72,7 @@ with createDB.app.app_context():
 
             creating_table_for = item["billing"]
             for index in range(len(creating_table_for["billing_id"])):
-                newBilling = createDB.ListingBillingInfo(listing_id=item["listing_id"],
-                                                         billing_id=creating_table_for["billing_id"][index],
-                                                         address_1=creating_table_for["address_1"][index],
+                newBilling = createDB.ListingBillingInfo(address_1=creating_table_for["address_1"][index],
                                                          address_2=creating_table_for["address_2"][index],
                                                          address_3=creating_table_for["address_3"][index],
                                                          city=creating_table_for["city"][index],
@@ -86,22 +80,28 @@ with createDB.app.app_context():
                                                          country_id=creating_table_for["country_id"][index],
                                                          state=creating_table_for["state"][index],
                                                          phone_area_code=creating_table_for["phone_area_code"][index],
-                                                         phone_num=creating_table_for["phone_number"][index],
+                                                         phone_num=creating_table_for["phone_num"][index],
                                                          payment_method=creating_table_for["payment_method"][index],
-                                                         billing_type="listing")
+                                                         billing_type="listing",
+                                                         listing_id=item["listing_id"])
 
             creating_table_for = item["payment_method"]
-            for index in range(len(creating_table_for["payment_id"])):
-                newPaymentMethod = createDB.CreditCardDetails(details_id=creating_table_for["payment_id"][index],
-                                                              payment_type=creating_table_for["transaction_type"][index],
-                                                              card_identifier=creating_table_for["card_identifier"][index],
+            for index in range(len(creating_table_for["payment_method"])):
+                newPaymentMethod = createDB.CreditCardDetails(payment_type=creating_table_for["transaction_type"][
+                                                                  index],
+                                                              identifier=creating_table_for["card_identifier"][index],
                                                               card_num=creating_table_for["card_num"][index],
-                                                              expiry_date=creating_table_for["expiry_date"][index],
+                                                              expiry_date=datetime.datetime.fromisoformat(
+                                                                  creating_table_for["expiry_date"][index]),
                                                               cardholder=creating_table_for["cardholder"][index])
-                newBilling.details.append(newPaymentMethod)
-                newListing.billing.append(newBilling)
+
+                newPaymentMethod.classification = createDB.CardClassification.query.filter_by(
+                    card_identifier=creating_table_for["card_identifier"][index]).first()
+
+                newListing.billing = [newBilling]
                 createDB.db.session.add(newPaymentMethod)
                 createDB.db.session.add(newBilling)
 
             createDB.db.session.add(newListing)
             createDB.db.session.commit()
+            createDB.db.session.close()
