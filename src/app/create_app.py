6957f2da3631d6bb9d542d1542b749968a664b95
Lavanya Kasturia <KasturiaL@cardiff.ac.uk>
2024-05-02 04:45:22 +0100

    Moved app creation to InitApp function

--- src/app/create_app.py
+++ src/app/create_app.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 import os
+from instance.db.db import db
 
 cur_path = os.path.dirname(__file__)
 src = os.path.abspath(os.path.join(cur_path, os.pardir))
 parent = os.path.abspath(os.path.join(src, os.pardir))
 config = os.path.join(parent, 'config', 'config.cfg')
 
-app = Flask(__name__)
-app.config.from_pyfile(config)
-bootstrap = Bootstrap(app)
+
+def InitApp():
+
+    app = Flask(__name__)
+    app.config.from_pyfile(config)
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{parent}/instance/app.db"
+    bootstrap = Bootstrap(app)
+    db.init_app(app)
+
+    return app
