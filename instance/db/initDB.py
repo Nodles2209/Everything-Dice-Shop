3d3a555f34ba87014944547444669f5caa594d74
Lavanya Kasturia <KasturiaL@cardiff.ac.uk>
2024-05-04 08:55:10 +0100

    Added account setting lookup table initialisation and refactored project structure

--- instance/db/initDB.py
+++ instance/db/initDB.py
@@ -34,6 +34,81 @@ def initLookupTables():
                                        card_type=vendor["card_type"])
         db.session.add(newVendor)
 
+    for setting in lookup_data["Account settings"]:
+        for option in setting["setting_options"]:
+            newOption = AccountSettings(setting_option=option,
+                                        is_anon=setting["is_anon"])
+            db.session.add(newOption)
+
+
+def initOptions(item, table_for, newListing):
+    for index in range(len(table_for["option_id"])):
+        newOption = ListingOptions(for_listing_id=item["listing_id"],
+                                   option_img=table_for["option_img"][index],
+                                   option_name=table_for["option_name"][index],
+                                   option_price=table_for["option_price"][index],
+                                   option_stock=table_for["option_stock"][index],
+                                   option_sold=table_for["option_sold"][index],
+                                   option_inStock=table_for["option_inStock"][index])
+        newListing.options.append(newOption)
+
+
+def initReviewsAndReplies(item, table_for, newListing):
+    for index in range(len(table_for["review_id"])):
+        newReview = ListingReviews(review_id=table_for["review_id"][index],
+                                   listing_id=item["listing_id"],
+                                   review_user_id=table_for["review_user_id"][index],
+                                   review_title=table_for["review_title"][index],
+                                   review_text=table_for["review_text"][index],
+                                   review_rating=table_for["review_rating"][index],
+                                   num_of_replies=table_for["num_of_replies"][index])
+
+    table_for = item["replies"]
+    for index in range(len(table_for["reply_id"])):
+        newReply = ReviewReplies(reply_id=table_for["reply_id"][index],
+                                 reply_user_id=table_for["reply_user_id"][index],
+                                 under_review_id=table_for["review_id"][index],
+                                 reply_text=table_for["reply_text"][index])
+        newReview.replies.append(newReply)
+        newListing.reviews.append(newReview)
+        db.session.add(newReply)
+        db.session.add(newReview)
+
+
+def initBillingAndPayment(item, table_for, newListing):
+    for index in range(len(table_for["billing_id"])):
+        newBilling = ListingBillingInfo(address_1=table_for["address_1"][index],
+                                        address_2=table_for["address_2"][index],
+                                        address_3=table_for["address_3"][index],
+                                        city=table_for["city"][index],
+                                        postcode=table_for["postcode"][index],
+                                        country_id=table_for["country_id"][index],
+                                        state=table_for["state"][index],
+                                        phone_area_code=table_for["phone_area_code"][
+                                            index],
+                                        phone_num=table_for["phone_num"][index],
+                                        payment_method=table_for["payment_method"][index],
+                                        billing_type="listing",
+                                        listing_id=item["listing_id"])
+
+    table_for = item["payment_method"]
+    for index in range(len(table_for["payment_method"])):
+        newPaymentMethod = CreditCardDetails(payment_type=table_for["transaction_type"][
+            index],
+                                             identifier=table_for["card_identifier"][
+                                                 index],
+                                             card_num=table_for["card_num"][index],
+                                             expiry_date=datetime.datetime.fromisoformat(
+                                                 table_for["expiry_date"][index]),
+                                             cardholder=table_for["cardholder"][index])
+
+        newPaymentMethod.classification = CardClassification.query.filter_by(
+            card_identifier=table_for["card_identifier"][index]).first()
+
+        newListing.billing = [newBilling]
+        db.session.add(newPaymentMethod)
+        db.session.add(newBilling)
+
 
 def initListings():
     items_file = os.path.join(jsonPath, 'all_items.json')
@@ -50,73 +125,12 @@ def initListings():
                                      num_of_reviews=item["num_of_reviews"],
                                      footprint=item["footprint"],
                                      sold=item["sold"],
-                                     in_stock=item["in_stock"])
+                                     in_stock=item["in_stock"],
+                                     date_added=datetime.datetime.fromisoformat(item["date_added"]))
 
-            creating_table_for = item["options"]
-            for index in range(len(creating_table_for["option_id"])):
-                newOption = ListingOptions(for_listing_id=item["listing_id"],
-                                           option_img=creating_table_for["option_img"][index],
-                                           option_name=creating_table_for["option_name"][index],
-                                           option_price=creating_table_for["option_price"][index],
-                                           option_stock=creating_table_for["option_stock"][index],
-                                           option_sold=creating_table_for["option_sold"][index],
-                                           option_inStock=creating_table_for["option_inStock"][index])
-                newListing.options.append(newOption)
-
-            creating_table_for = item["reviews"]
-            for index in range(len(creating_table_for["review_id"])):
-                newReview = ListingReviews(review_id=creating_table_for["review_id"][index],
-                                           listing_id=item["listing_id"],
-                                           review_user_id=creating_table_for["review_user_id"][index],
-                                           review_title=creating_table_for["review_title"][index],
-                                           review_text=creating_table_for["review_text"][index],
-                                           review_rating=creating_table_for["review_rating"][index],
-                                           num_of_replies=creating_table_for["num_of_replies"][index])
-
-            creating_table_for = item["replies"]
-            for index in range(len(creating_table_for["reply_id"])):
-                newReply = ReviewReplies(reply_id=creating_table_for["reply_id"][index],
-                                         reply_user_id=creating_table_for["reply_user_id"][index],
-                                         under_review_id=creating_table_for["review_id"][index],
-                                         reply_text=creating_table_for["reply_text"][index])
-                newReview.replies.append(newReply)
-                newListing.reviews.append(newReview)
-                db.session.add(newReply)
-                db.session.add(newReview)
-
-            creating_table_for = item["billing"]
-            for index in range(len(creating_table_for["billing_id"])):
-                newBilling = ListingBillingInfo(address_1=creating_table_for["address_1"][index],
-                                                address_2=creating_table_for["address_2"][index],
-                                                address_3=creating_table_for["address_3"][index],
-                                                city=creating_table_for["city"][index],
-                                                postcode=creating_table_for["postcode"][index],
-                                                country_id=creating_table_for["country_id"][index],
-                                                state=creating_table_for["state"][index],
-                                                phone_area_code=creating_table_for["phone_area_code"][
-                                                    index],
-                                                phone_num=creating_table_for["phone_num"][index],
-                                                payment_method=creating_table_for["payment_method"][index],
-                                                billing_type="listing",
-                                                listing_id=item["listing_id"])
-
-            creating_table_for = item["payment_method"]
-            for index in range(len(creating_table_for["payment_method"])):
-                newPaymentMethod = CreditCardDetails(payment_type=creating_table_for["transaction_type"][
-                    index],
-                                                     identifier=creating_table_for["card_identifier"][
-                                                         index],
-                                                     card_num=creating_table_for["card_num"][index],
-                                                     expiry_date=datetime.datetime.fromisoformat(
-                                                         creating_table_for["expiry_date"][index]),
-                                                     cardholder=creating_table_for["cardholder"][index])
-
-                newPaymentMethod.classification = CardClassification.query.filter_by(
-                    card_identifier=creating_table_for["card_identifier"][index]).first()
-
-                newListing.billing = [newBilling]
-                db.session.add(newPaymentMethod)
-                db.session.add(newBilling)
+            initOptions(item, item["options"], newListing)
+            initReviewsAndReplies(item, item["reviews"], newListing)
+            initBillingAndPayment(item, item["billing"], newListing)
 
             db.session.add(newListing)
 
