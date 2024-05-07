e7ea246293ac9e4ffdee7beb1f70043f879c8646
Lavanya Kasturia <KasturiaL@cardiff.ac.uk>
2024-05-07 10:04:26 +0100

    Moved initial csrf protect variable outside of function to be accessible

--- src/app/create_app.py
+++ src/app/create_app.py
@@ -5,14 +5,15 @@ from flask_wtf import CSRFProtect
 from instance.db.sqlalchemyDB import db
 from src.app import getSQLPath, getConfig
 
+csrf = CSRFProtect()
+
 
 def InitApp():
-
     app = Flask(__name__)
     app.config.from_pyfile(getConfig())
     app.config["SQLALCHEMY_DATABASE_URI"] = getSQLPath()
     bootstrap = Bootstrap(app)
-    csrf = CSRFProtect(app)
     db.init_app(app)
+    csrf.init_app(app)
 
     return app
