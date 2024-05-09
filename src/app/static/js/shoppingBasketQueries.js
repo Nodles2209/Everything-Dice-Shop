6ba2e775267c057b898960b6721345d0a2338dfb
Lavanya Kasturia <KasturiaL@cardiff.ac.uk>
2024-05-09 08:37:25 +0100

    Fixed basket to delete or update the right item when duplicate items with the same option exist

--- src/app/static/js/shoppingBasketQueries.js
+++ src/app/static/js/shoppingBasketQueries.js
@@ -1,24 +1,26 @@
 const csrf_token = "{{ csrf_token() }}";
 $.ajaxSetup({
-        beforeSend: function(xhr, settings) {
-            if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
-                xhr.setRequestHeader("X-CSRFToken", csrf_token);
-            }
+    beforeSend: function(xhr, settings) {
+        if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrf_token);
         }
-    });
+    }
+});
 
 axios.defaults.headers.common["X-CSRFToken"] = "{{ csrf_token() }}";
 
-function updateIndividualPrice(optionId, newPrice) {
-    let individualPriceDisplay = document.getElementById(optionId);
-
-    individualPriceDisplay.textContent = '£' + newPrice.toFixed(2);
+function updateIndividualPrice(basketItemNum, newPrice) {
+    let individualPriceDisplay = document.getElementById(basketItemNum);
+    if (individualPriceDisplay) {
+        individualPriceDisplay.textContent = '£' + newPrice.toFixed(2);
+    }
 }
 
 function updatePriceOnChange(quantityField) {
     let itemContainer = quantityField.closest('.basket-item');
     let priceField = itemContainer.querySelector('.price-field');
-    let optionId = itemContainer.querySelector('.hidden-field input').value;
+    let optionId = itemContainer.querySelector('.hidden-field input[name="hidden-option-id"]').value;
+    let basketItemNum = itemContainer.querySelector('.hidden-field input[name="hidden-basket-id"]').value;
     let newQuantity = parseInt(quantityField.value);
 
     fetch('/get_price/' + optionId)
@@ -27,9 +29,9 @@ function updatePriceOnChange(quantityField) {
         let newPrice = parseFloat(price) * newQuantity;
         priceField.value = '£' + newPrice.toFixed(2);
 
-
-        updateIndividualPrice(optionId, newPrice);
+        updateIndividualPrice(basketItemNum, newPrice);
         updateTotalPrice();
+        updateBasketItem(optionId, newQuantity, basketItemNum);
     })
     .catch(error => {
         console.error('Error fetching price:', error);
@@ -43,19 +45,17 @@ quantityFields.forEach(function(quantityField) {
     });
 });
 
-function updateBasketItem(optionId, quantity) {
+function updateBasketItem(optionId, quantity, basketItemNum) {
     let formData = new FormData();
     formData.append('quantity', quantity);
-
-    console.log(csrf_token);
-
+    formData.append('basket_item_num', basketItemNum);
 
     axios.post('/basket/update/' + optionId, formData, {
-            headers: {
-                'Content-Type': 'multipart/form-data',
-                'X-CSRFToken': csrf_token
-            }
-        })
+        headers: {
+            'Content-Type': 'multipart/form-data',
+            'X-CSRFToken': csrf_token
+        }
+    })
     .then(response => {
         if (response.ok) {
             updateTotalPrice();
@@ -76,16 +76,17 @@ function updateTotalPrice() {
         const priceText = element.value.replace('£', '').replace(',', '');
         const price = parseFloat(priceText);
         totalPrice += price;
-}
+    }
 
-document.querySelector('.total-price .formatted-total-price').textContent = '£' + totalPrice.toFixed(2);
+    document.querySelector('.total-price .formatted-total-price').textContent = '£' + totalPrice.toFixed(2);
 }
 
 const itemForms = document.querySelectorAll('.change-item-form');
 for (const element of itemForms) {
     element.addEventListener('change', function(event) {
-        let optionId = event.target.closest('.change-item-form').querySelector('.hidden-field input').value;
+        let optionId = event.target.closest('.change-item-form').querySelector('.hidden-field input[name="hidden-option-id"]').value;
         let quantity = event.target.value;
-        updateBasketItem(optionId, quantity);
+        let basketItemNum = event.target.closest('.change-item-form').querySelector('.hidden-field input[name="hidden-basket-id"]').value;
+        updateBasketItem(optionId, quantity, basketItemNum);
     });
 }
